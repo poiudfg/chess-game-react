f13869cadd867554b1b01a46b54a6d66eb7494cf
poiu <94011063+poiudfg@users.noreply.github.com>
2023-04-18 20:57:34 +0700

    Add

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chessboard from './components/Chessboard';
+
+function App() {
+  return (
+    <div className='App'>
+      <Chessboard/>
+    </div>
+  );
+}
+
+export default App;
