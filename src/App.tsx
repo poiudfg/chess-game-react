fdba78f86d7769ea798ccbf5210369ac80cb4e7f
poiu <94011063+poiudfg@users.noreply.github.com>
2023-04-19 22:33:53 +0700

    Add

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Chessboard from './components/Chessboard';
+
+function App() {
+  return (
+    
+    <div className='App'>
+      <Chessboard/>
+      <div className='dottt'></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './Chessboard.css'
+
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export default function Chessboard() {
+    let board = [];
+
+    for (let y = verticalAxis.length - 1; y >= 0; y--) {
+      for (let x = 0; x < horizontalAxis.length; x++) {
+          const number = y + x + 2;
+  
+          if(number % 2 === 0) {
+              board.push(
+                  <div className="tile black-tile">[{horizontalAxis[x]}{verticalAxis[y]}]</div>
+                );
+          } else {
+              board.push(
+                  <div className="tile white-tile">[{horizontalAxis[x]}{verticalAxis[y]}]</div>
+                );
+          }
+      }
+    }
+
+    
+    return (
+      
+      <div className='monitor'>
+      <div className='section_chess'> Chess Game</div>
+      <div id="chessboard">{board}</div>
+      </div>
+      
+    )
+}
+
